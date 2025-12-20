4f47cf88729caf3941399b49f734ed552a954ca8
Menisy2004 <135966632+Menisy2004@users.noreply.github.com>
2025-12-20 11:31:12 +0200

    Configure page and add sidebar options
    Added page configuration and sidebar navigation.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,20 @@
 import streamlit as st
 
+st.set_page_config(page_title='Face Detection Project', layout='wide')
 st.title('😄 Face detection Model')
 
 st.info('This app builds a machine learning model!')
+
+section = st.sidebar.radio(
+    "Go to",
+    [
+        "Introduction",
+        "Dataset",
+        "Model Architecture",
+        "Training",
+        "Results",
+        "Live Demo",
+        "Conclusion"
+    ]
+)
+
