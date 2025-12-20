7fe87df855bc031c829771e22b4c3866626d0c78
Abdelrahman294 <abdelrahman.badr294@gmail.com>
2025-12-20 19:58:01 +0200

    Update VGG16 architecture image file path

--- streamlit_app.py
+++ streamlit_app.py
@@ -237,7 +237,7 @@ if section == "Model Architecture":
 
     # VGG16 Architecture Image
     st.image(
-        "images/vgg16_architecture.png",
+        "vgg-16.bak.png",
         caption="VGG16 convolutional backbone",
         use_column_width=True
     )
