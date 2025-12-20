ee8a79cca38a4792bb8ead539ff908b7b01a7b08
Menisy2004 <135966632+Menisy2004@users.noreply.github.com>
2025-12-20 11:53:36 +0200

    Remove info message from the app
    Removed info message about the machine learning model.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,6 @@ import streamlit as st
 st.set_page_config(page_title='Face Detection Project', layout='wide')
 st.title('😄 Face detection Model')
 
-st.info('This app builds a machine learning model!')
-
 section = st.sidebar.radio(
     "Go to",
     [
